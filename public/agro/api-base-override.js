d1dd6792a8177bc8cfa2e263d802a84393b9da6a
DMontgomery40 <dmontg@gmail.com>
2025-10-14 05:08:44 -0600

    AGRO: fix fetch shims in build (regex + /agro-api/api de-dupe)

--- public/agro/api-base-override.js
+++ public/agro/api-base-override.js
@@ -23,12 +23,12 @@
           try {
             // String URL
             if (typeof input === 'string') {
-              if (input.startsWith('/agro-api/api/')) input = '/agro-api' + input.slice('/agro-api'.__len__() + 4);
+              if (input.startsWith('/agro-api/api/')) input = '/agro-api' + input.slice('/agro-api'.length + 4);
               else if (input.startsWith('/api/')) input = '/agro-api' + input.slice(4);
               else if (/^https?:///i.test(input)) {
                 const u = new URL(input);
                 if (u.origin === window.location.origin) {
-                  if (u.pathname.startsWith('/agro-api/api/')) u.pathname = '/agro-api' + u.pathname.slice('/agro-api'.__len__() + 4);
+                  if (u.pathname.startsWith('/agro-api/api/')) u.pathname = '/agro-api' + u.pathname.slice('/agro-api'.length + 4);
                   else if (u.pathname.startsWith('/api/')) u.pathname = '/agro-api' + u.pathname.slice(4);
                   input = u.toString();
                 }
